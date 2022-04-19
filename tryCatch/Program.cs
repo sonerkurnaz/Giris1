3bdcc08536228cb68a4f5311aa80af099d073a56
Soner Kurnaz <sonerkurnaz@outlook.com>
2022-04-19 12:20:22 +0300

    trycatch

--- tryCatch/Program.cs
+++ tryCatch/Program.cs
@@ -6,7 +6,61 @@ namespace tryCatch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region Ornek
+            //int sayi1, sayi2;
+            //float sonuc;
+            //try
+            //{
+            //    Console.Write("1.Sayıyı giriniz: ");
+            //    sayi1 = int.Parse(Console.ReadLine());
+
+            //    Console.Write("2.Sayıyı giriniz: ");
+            //    sayi2 = int.Parse(Console.ReadLine());
+
+            //    sonuc = sayi1 / sayi2;
+            //    Console.WriteLine("Sonuc" + sonuc);
+            //}
+            //catch (OverflowException ex)
+            //{
+            //    Console.WriteLine("Daha kücük sayi giriniz.");
+            //}
+            //catch (DivideByZeroException ex)
+            //{
+            //    Console.WriteLine("Sifira bolme hatasi");
+            //}
+            //catch (FormatException ex)
+            //{
+            //    Console.WriteLine("Düzgün veri giriniz. Girdiğiniz veri sayisal degil");
+            //}
+            //catch (Exception ex)
+            //{
+            //    //Hata oluştu: Value was either too large or too small for an Int32.
+            //    //Hata oluştu: Attempted to divide by zero.
+            //    //Hata oluştu: Input string was not in a correct format.
+            //    Console.WriteLine("Hata oluştu: " + ex.Message);
+            //}
+            #endregion
+            #region Ornek2
+            DateTime dogumTarihi;
+            string input;
+            try
+            {
+                Console.Write("Dogum tarihinizi giriniz:");
+                input = Console.ReadLine();
+                dogumTarihi= DateTime.Parse(input);
+                Console.WriteLine(dogumTarihi.ToString());
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Girilen deger dogru formatta degil.");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            #endregion
+            Console.ReadLine();
         }
     }
 }
