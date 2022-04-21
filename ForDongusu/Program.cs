3b5a2075f253e5cafadd2f67de98de05df2ca47f
Soner Kurnaz <sonerkurnaz@outlook.com>
2022-04-21 10:46:54 +0300

    breakContinueReturn

--- ForDongusu/Program.cs
+++ ForDongusu/Program.cs
@@ -80,23 +80,52 @@ namespace ForDongusu
             //}
             #endregion
             #region Ornek5
-            Console.WriteLine(DateTime.Now);
-            Console.WriteLine("Yıl"+DateTime.Now.Year);
-            Console.WriteLine("Ay"+DateTime.Now.Month);
-            Console.WriteLine("Gün"+DateTime.Now.Day);
-            Console.WriteLine("Haftanın kacıncı günü"+DateTime.Now.DayOfWeek);
-            Console.WriteLine("Yılın kacıncı günü"+DateTime.Now.DayOfYear);
+            //Console.WriteLine(DateTime.Now);
+            //Console.WriteLine("Yıl: " + DateTime.Now.Year);
+            //Console.WriteLine("Ay: " + DateTime.Now.Month);
+            //Console.WriteLine("Gün: " + DateTime.Now.Day);
+            //Console.WriteLine("Haftanın kacıncı günü: " + DateTime.Now.DayOfWeek);
+            //Console.WriteLine("Yılın kacıncı günü: " + DateTime.Now.DayOfYear);
 
-            for (int i = 2010; i <= DateTime.Now.Year; i++)
+            //for (int i = 2010; i <= DateTime.Now.Year; i++)
+            //{
+            //    //pandemi yıllarını es geçelim 2020-2021
+
+            //    if (!(i==2020 || i==2021))
+            //    {
+            //        continue;
+
+            //        Console.WriteLine(i);
+
+            //    }
+
+            //}
+            #endregion
+            #region Ornek6
+            for (int i = 0; i < 100; i++)
             {
-                //pandemi yıllarını es geçelim 2020-2021
-                
-                if (!(i==2020 || i==2021))
-                {
+                if (i % 12 != 0)
+
+                    continue;
+                else
                     Console.WriteLine(i);
-                }
+
             }
             #endregion
+            #region Break,Continue,Return
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 1)
+                    //continue;
+                    //break;
+                    return;
+
+
+            }
+            #endregion
+
+           
+
 
         }
     }
