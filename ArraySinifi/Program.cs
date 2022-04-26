b7d73f20dfac9cef9b01d64f0219dc2d828f3bd4
Soner Kurnaz <sonerkurnaz@outlook.com>
2022-04-26 11:27:59 +0300

    arraySinifi , arrayList

---
+++ ArraySinifi/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ArraySinifi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] arabalar = { "Bmw", "Audi", "Fiat",
+            //    "Toyota", "Honda", "Alfa Romeo", "Subaru" };
+            //Console.WriteLine("Aradığınız araç?");
+            //string arananAraba = Console.ReadLine();
+            //int index = Array.IndexOf(arabalar, arananAraba);
+
+            //if (index == -1)
+            //{
+            //    Console.WriteLine("Aradığınız" + arananAraba + "listede yoktur.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Aradığınız" + arananAraba + " " + index + ".sıradadır.");
+            //}
+            //index = Array.IndexOf(arabalar, arananAraba,2);
+
+            #region Array.Reverse
+            string[] arabalar = { "Bmw", "Audi", "Fiat",
+                "Toyota", "Honda", "Alfa Romeo", "Subaru" };
+            Array.Sort(arabalar);
+            Array.Reverse(arabalar);
+            foreach(var item in arabalar)
+            {
+                Console.WriteLine(item);    
+
+            }
+            
+            #endregion
+
+
+        }
+    }
+}
