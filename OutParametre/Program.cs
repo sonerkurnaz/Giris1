224b352d012f1211f79b3b7b78b2ef4c66a958c3
Soner Kurnaz <sonerkurnaz@outlook.com>
2022-05-09 11:00:56 +0300

    parametrelimetotlar ve out parametre

---
+++ OutParametre/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace OutParametre
+{
+    internal class Program
+    {
+        /*
+     * OUT ANAHTAR KELİMESİ
+     * Bir metot icerisinden disariya birden fazla deger dondurmek 
+     * isterseniz, "return" anahtar kelimesi fonksiyonlarda size 
+     * yetmeyecektir. Void metotlar ise zaten geriye deger dondurmez. 
+     * Bu bilinenleri yikabilmek icin kullandigimiz anahtar 
+     * kelime "out" anahtar kelimesidir...
+     */
+
+
+        static void Main(string[] args)
+        {
+
+            double yakalananSonuc;
+
+            geriyeDegerDonduren(12, out yakalananSonuc);
+
+            Console.WriteLine(yakalananSonuc.ToString());
+
+
+            int toplamaSonucu;
+            int farkSonucu;
+            long carpimSonucu;
+            double bolumSonucu;
+            byte modSonucu;
+
+            Hesapla(9, 4,
+                out toplamaSonucu,
+                out farkSonucu,
+                out carpimSonucu,
+                out bolumSonucu,
+                out modSonucu);
+            Console.WriteLine("Toplam:" + toplamaSonucu);
+            Console.WriteLine("Fark:" + farkSonucu);
+            Console.WriteLine("Carpim:" + carpimSonucu);
+            Console.WriteLine("Bolum:" + bolumSonucu);
+
+
+
+
+        }
+
+
+        public static void geriyeDegerDonduren(int sayi, out double firlatilanSonuc)
+        {
+            firlatilanSonuc = Math.Pow(sayi, 2);
+        }
+
+        public static void Hesapla(int sayiBir,
+            int sayiIki,
+            out int toplam,
+            out int fark,
+            out long carpim,
+            out double bolum,
+            out byte mod)
+        {
+            toplam = sayiBir + sayiIki;
+            fark = sayiBir - sayiIki;
+            carpim = sayiBir * sayiIki;
+            bolum = sayiBir / sayiIki;
+            mod = (byte)(sayiBir % sayiIki);
+        }
+
+    }
+}
--- ParametreliMetodlar/Program.cs
+++ ParametreliMetodlar/Program.cs
@@ -3,36 +3,95 @@
 namespace ParametreliMetodlar
 {
     internal class Program
-    {
+    { /*
+            PARAMETRELİ METOTLAR
+            Disaridan gelecek olan degerlere gore islem yonunu degistirecek olan metotlardir...
+            Parametredeki degiskene, iceride deger atanmaz!
+            Metot prensipleri geregi, parametrenin nereden gelecegi soylenmez....
+        */
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            EkranaYazdir("Hello World!");
-            TurkceHarfler("şemsi paşa pasajında sesi büzüşesiciler");
-            //verilen kelimeyi harf harf bastıran metod.
+            // EkranaYazdir("Hello World");
+            // Verilen kelimeyi harf harf ekrana bastiran metod.
 
-            //Verilen string içerisindeki türkçe
-            //harflerin sayısını bulan metod.
+            // Verilen string icerisinde ki turkce harflerin
+            // sayisi bulan metod..
+            //SesliHarfSayisi("şemsi paşa pasajında sesi büzüşeciler");
+
+            // 1- Rastgele Sifre üreten metod yaziniz. 
+            // En az 1 tane buyuk harf , en az 1 sayi olacak ve
+            // en az 8 karakter uzunlugunda olacak
+
+            // Verilen sayinin asal sayi olup olmadigini belirleyen metod 
+            Console.WriteLine(Topla(1, 2, 3, 4, 5, 6, 7, 8, 9));
+            Console.WriteLine(Topla(1, 2, 3, 4, 5, 8, 6, 7, 8, 9, 10, 11, 12));
         }
 
-        static void EkranaYazdir(string mesaj)
+        /// <summary>
+        /// Verilen Sayilari Toplar
+        /// </summary>
+        /// <param name="sayi1">int tipinde</param>
+        /// <param name="sayi2">int tipinde</param>
+        /// <returns>int tipinde</returns>
+        public static int Topla(int sayi1, int sayi2)
         {
-            foreach (char harf in mesaj)
+            return sayi1 + sayi2;
+        }
+        public static int Topla(int sayi1, float sayi2)
+        {
+            return sayi1 + (int)sayi2;
+        }
+        /// <summary>
+        /// Uç Parametreli metod
+        /// </summary>
+        /// <param name="sayi1"></param>
+        /// <param name="sayi2"></param>
+        /// <param name="sayi3"></param>
+        /// <returns></returns>
+        public static int Topla(int sayi1, int sayi2, int sayi3)
+        {
+            return sayi1 + sayi2 + sayi3;
+        }
+
+        public static int Topla(int sayi1, int sayi2, int sayi3, int sayi4)
+        {
+            return sayi1 + sayi2 + sayi3 + sayi4;
+        }
+        /// <summary>
+        /// Sonsuz parametreli metod
+        /// </summary>
+        /// <param name="parametreler">parametreler bir int dizisidir</param>
+        /// <returns></returns>
+        public static int Topla(params int[] parametreler)
+        {
+            int toplam = 0;
+            foreach (var item in parametreler)
             {
-                Console.WriteLine(harf);
+                toplam += item;
+            }
+            return toplam;
+        }
+
+        public static void DavetiyeGonder(params string[] davetliler)
+        {
+            foreach (var davetli in davetliler)
+            {
+                Console.WriteLine(davetli);
             }
         }
-        public static void TurkceHarfler(string cumle)
+        public static void SesliHarfSayisi(string cumle)
         {
-            char[] turkceHarf = { 'ç', 'ı', 'ğ', 'ş', 'ö', 'ü' };
+            // Turkçe harfler : ç,ı,ğ,ş,ö,ü
+            char[] turkceHarfler = { 'ç', 'ı', 'ğ', 'ş', 'ö', 'ü' };
             int[] turkceHarfSayisi = { 0, 0, 0, 0, 0, 0 };
-            foreach (char harf in cumle)
+            foreach (var harf in cumle)
             {
                 if (harf == 'ç')
                     turkceHarfSayisi[0]++;
                 if (harf == 'ı')
                     turkceHarfSayisi[1]++;
-                if (harf =='ğ')
+                if (harf == 'ğ')
                     turkceHarfSayisi[2]++;
                 if (harf == 'ş')
                     turkceHarfSayisi[3]++;
@@ -42,9 +101,18 @@ namespace ParametreliMetodlar
                     turkceHarfSayisi[5]++;
 
             }
+
             for (int i = 0; i < turkceHarfSayisi.Length; i++)
             {
-                Console.WriteLine(turkceHarf[i]+" :"+turkceHarfSayisi[i]);
+                Console.WriteLine(turkceHarfler[i] + " :" + turkceHarfSayisi[i]);
+            }
+
+        }
+        static void EkranaYazdir(string mesaj)
+        {
+            foreach (char harf in mesaj)
+            {
+                Console.WriteLine(harf);
             }
         }
     }
