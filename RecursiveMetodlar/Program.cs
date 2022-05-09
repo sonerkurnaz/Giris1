b5982c405efba17d2629457567e9095bfb507c7d
Soner Kurnaz <sonerkurnaz@outlook.com>
2022-05-09 12:48:03 +0300

    recursive

---
+++ RecursiveMetodlar/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace RecursiveMetodlar
+{
+    internal class Program
+    {
+        // bir fonksiyonun içinde yine kendisinden bir parça
+        // bulunuyorsa bu fonksiyonlara özyineli (recursive)
+        // fonksiyon denilir -  metot içerisinde kendisini
+        // tekrardan çağırma işlemi
+
+        static void Main(string[] args)
+        {
+
+            // Bir sayinin faktoryelini nasil aliriz.
+
+            // 5! = 5x4x3x2x1
+
+            // FaktoryelHesapla(5);
+            //RecursiveFaktoryel(3);
+            //Console.WriteLine("Hello World!");
+
+            //fibanocci sayilari 
+            // 0,1,2,3,5,8,13,21,34,55....
+            // fibanocci(100);
+            Console.WriteLine(Fib_Recursive(20));
+        }
+
+
+        public static long Fib(int n)
+        {
+            if (n < 2)
+                return n;
+            long[] f = new long[n + 1];
+            f[0] = 0;
+            f[1] = 1;
+
+            for (int i = 2; i <= n; i++)
+            {
+                f[i] = f[i - 1] + f[i - 2];
+                Console.WriteLine(f[i]);
+            }
+            return f[n];
+        }
+
+        public static long Fib_Recursive(int n)
+        {
+            if (n == 0 || n == 1)
+                return n;
+            return Fib(n - 2) + Fib(n - 1);
+        }
+
+
+        public static void fibanocci(int sayi)
+        {
+            decimal[] fib = new decimal[sayi];
+            fib[0] = 0;
+            fib[1] = 1;
+            for (int i = 2; i < sayi; i++)
+            {
+                fib[i] = fib[i - 1] + fib[i - 2];
+                Console.Write("i= " + i + " =>" + fib[i]);
+                Console.WriteLine("  Oran :" + fib[i] / (fib[i - 1]));
+            }
+
+
+        }
+        public static void FaktoryelHesapla(int sayi)
+        {
+            long sonuc = 1;
+            if (sayi > 1)
+            {
+                for (int i = 1; i <= sayi; i++)
+                {
+                    sonuc *= i;
+                }
+            }
+
+        }
+        public static long RecursiveFaktoryel(int sayi)
+        {
+            if (sayi == 0)
+                return 1;
+            else
+                return sayi * RecursiveFaktoryel(sayi - 1);
+        }
+    }
+}
---
+++ RefParametre/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace RefParametre
+{
+    internal class Program
+    { /*
+                REF ANAHTAR KELİMESİ
+        ref parametre niteleyicisi C#’ın “değer olarak çağrı” 
+        yerine “referans olarak çağrı” oluşturmasını sağlar. 
+        ref kullanarak, void tipli methodlardan değer alabilirsiniz.
+        Üstelik bu değeri alabilmek için yeni bir değişken atamanıza 
+        gerek bile kalmaz. ref, methoda parametre olarak soktuğunuz,
+        elinizdeki varolan değişkeni değiştirebilir.
+        */
+        static void Main(string[] args)
+        {
+            string[] elemanlar = new string[3];
+
+            elemanlar[0] = "ali@gmail.com";
+            elemanlar[1] = "veli@gmail.com";
+            elemanlar[2] = "ayse@hotmail.com";
+
+            //Console.Write("Mail Adresi: ");
+            //string mailAddress = Console.ReadLine();
+
+            MailGonder(ref elemanlar, "ercan@gmail.com");
+
+            //foreach (string eleman in elemanlar)
+            //{
+            //    Console.WriteLine(eleman);
+            //}
+        }
+        public static void MailGonder(ref string[] mailadresleri, string gonderen)
+        {
+            foreach (var item in mailadresleri)
+            {
+                Console.WriteLine(item + " adresinde " + gonderen + "  tarafindan mail gonderilmistir.");
+            }
+        }
+
+
+        public static void MailListecisi(ref string[] HangiDiziyeEklensin, string VerilerNeredenGeliyor)
+        {
+            // ; gordugumuz yerden degerleri bolup, diziye ekleyelim...
+            string[] gelenDegerler = VerilerNeredenGeliyor.Split(';');
+
+            for (int i = 0; i < gelenDegerler.Length; i++)
+            {
+                Array.Resize<string>(ref HangiDiziyeEklensin, HangiDiziyeEklensin.Length + 1);
+                HangiDiziyeEklensin[i] = gelenDegerler[i];
+            }
+        }
+    }
+}
